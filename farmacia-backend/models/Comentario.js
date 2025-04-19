511bd11c132e1115af11e975544a565deeca889b
HermanEcheverria <hecheverria@unis.edu.gt>
2025-04-19 16:55:08 -0600

    jsdocfix

--- farmacia-backend/models/Comentario.js
+++ farmacia-backend/models/Comentario.js
@@ -2,21 +2,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 /**
- * Esquema para los comentarios.
- * 
  * @typedef {Object} Comentario
- * @property {Schema.Types.ObjectId} user - ID del usuario que realizó el comentario.
- * @property {Schema.Types.ObjectId} medicamento - ID del medicamento comentado.
+ * @property {string} user - ID del usuario que realizó el comentario.
+ * @property {string} medicamento - ID del medicamento comentado.
  * @property {string} texto - Texto del comentario.
- * @property {Schema.Types.ObjectId} parentId - ID del comentario padre (si es una respuesta).
+ * @property {string|null} parentId - ID del comentario padre (si es una respuesta).
  * @property {Date} fecha - Fecha del comentario.
  */
-const ComentarioSchema = new Schema({
+
+/**
+ * Modelo de comentario para MongoDB usando Mongoose.
+ * @type {mongoose.Model<Comentario>}
+ */
+const Comentario = mongoose.model("Comentario", new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   medicamento: { type: Schema.Types.ObjectId, ref: "Medicamento", required: true },
   texto: { type: String, required: true },
   parentId: { type: Schema.Types.ObjectId, ref: "Comentario", default: null },
   fecha: { type: Date, default: Date.now }
-});
+}));
 
-module.exports = mongoose.model("Comentario", ComentarioSchema);
+module.exports = Comentario;
